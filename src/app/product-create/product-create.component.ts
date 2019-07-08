7a4604de6eb96876d02253bf65c1a57b2d131a34
AayaHassanOral4D <aayahassan94@gmail.com>
2019-07-08 22:16:19 +0300

    creating the form with all the validation rules

---
+++ src/app/product-create/product-create.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+  productForm: FormGroup;
+  constructor(private fb: FormBuilder, private ps: ProductsService) {
+    this.createForm();
+   }
+ 
+ createForm() {
+    this.productForm = this.fb.group({
+      ProductName: ['', Validators.required ],
+      ProductDescription: ['', Validators.required ],
+      ProductPrice: ['', Validators.required ]
+    });
+  }
+
+  addProduct(ProductName, ProductDescription, ProductPrice) {
+    this.ps.addProduct(ProductName, ProductDescription, ProductPrice);
+  }
+
+  ngOnInit() {
+  }
+
+}
