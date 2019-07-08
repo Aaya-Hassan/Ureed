51f57303c621c03c5c0136be409b1f9114819f39
AayaHassanOral4D <aayahassan94@gmail.com>
2019-07-08 22:30:05 +0300

    update the form fields data

---
+++ src/app/product-update/product-update.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+    updateProductForm: FormGroup;
+    product: any = {};
+  constructor(private route: ActivatedRoute, private router: Router, private ps: ProductsService, private fb: FormBuilder) {
+        this.createForm();
+   }
+   
+  createForm() {
+    this.updateProductForm = this.fb.group({
+      ProductName: ['', Validators.required ],
+      ProductDescription: ['', Validators.required ],
+      ProductPrice: ['', Validators.required ]
+    });
+  }
+
+  updateProduct(ProductName, ProductDescription, ProductPrice, id) {
+    this.route.params.subscribe(params => {
+      this.ps.updateProduct(ProductName, ProductDescription, ProductPrice, params.id);
+      this.router.navigate(['products']);
+    });
+  }
+  
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+        this.ps.editProduct(params['id']).subscribe(res => {
+          this.product = res;
+      });
+    });
+  }
+
+}
